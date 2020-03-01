f2ab174c4cd80865b8498d1ed4f5a4378095cea6
Jani <jani@xps>
2020-03-01 15:23:06 +0200

    Remove header and logo

---
+++ src/components/Customerlist.js
@@ -0,0 +1,94 @@
+import React, { useState, useEffect } from 'react'
+import ReactDOM from 'react-dom'
+import MaterialTable from 'material-table'
+
+import { forwardRef } from 'react';
+
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
+
+const tableIcons = {
+    Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+    Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+    DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+    Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+    Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+    SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+    ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+  };
+
+export default function Customerlist() {
+  const [customers, setCustomers] = useState([])
+
+  useEffect(() => fetchData(), [])
+
+  const fetchData = () => {
+    fetch('https://customerrest.herokuapp.com/api/customers')
+    .then(response => response.json())
+    .then(data => setCustomers(data.content))
+  }
+
+  const columns = [
+    {
+      title: "First name",
+      field: "firstname"
+    }, 
+    {
+      title: "Last name",
+      field: "lastname"
+    }, 
+    {
+      title: "Street address",
+      field: "streetaddress"
+    }, 
+    {
+      title: "Postal code",
+      field: "postcode"
+    }, 
+    {
+      title: "City",
+      field: "city"
+    }, 
+    {
+      title: "Email",
+      field: "email"
+    }, 
+    {
+      title: "Phone",
+      field: "phone"
+    }
+  ]
+
+  return (
+    <MaterialTable
+      columns={columns}
+      data={customers}
+      icons={tableIcons}
+      title="Customers"
+    />
+
+  )
+}
